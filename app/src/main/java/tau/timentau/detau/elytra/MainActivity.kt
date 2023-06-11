185442194c572a12f4db2b4f1944a6ede59f9f55
aleborgi <alexborgese01@gmail.com>
2023-06-11 20:15:53 +0200

    inizio schermata Login

--- app/src/main/java/tau/timentau/detau/elytra/MainActivity.kt
+++ app/src/main/java/tau/timentau/detau/elytra/MainActivity.kt
@@ -2,10 +2,13 @@ package tau.timentau.detau.elytra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tau.timentau.detau.elytra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
