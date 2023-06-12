8115b00df845a4174ff52b2b2fa2d277dcabba97
thomas <thomasherondale@gmail.com>
2023-06-12 21:33:08 +0200

    validazione password e conferma

--- app/src/main/java/tau/timentau/detau/elytra/RegisterFragment.kt
+++ app/src/main/java/tau/timentau/detau/elytra/RegisterFragment.kt
@@ -5,18 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.StringRes
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import tau.timentau.detau.elytra.database.Repository
 import tau.timentau.detau.elytra.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
 
     private lateinit var binding: FragmentRegisterBinding
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentRegisterBinding.inflate(layoutInflater)
+
+        binding.mailText.editText?.setOnFocusChangeListener { _, _ -> validateMailField() }
+        binding.passText.editText?.setOnFocusChangeListener { _, _ -> validatePasswordField() }
+        binding.confirmText.editText?.setOnFocusChangeListener { _, _ -> validateConfirmField() }
 
         return binding.root
     }
@@ -32,8 +40,46 @@ class RegisterFragment : Fragment() {
             else
                 binding.mailText.error = null
 
+        coroutineScope.launch {
+            try {
+                val isEmailUsed = Repository.isMailUsed(binding.mailText.text).await()
+                if (isEmailUsed)
+                    binding.mailText.error = getString(R.string.mail_in_uso)
+            } catch (_: Exception) {
+                // ignora le eccezioni, il controllo sarà bloccante una volta premuto registrati
+            }
+        }
+
         // messaggio di errore vuoto -> campo corretto
         return errorTextRes == null
+    }
+
+    private fun validatePasswordField(): Boolean {
+        binding.passText.text.let {
+            binding.passText.error = when {
+                (it.length !in 6..18) ->
+                    getString(R.string.pwd_corta)
+                it.contains(Regex("\\s")) ->
+                    getString(R.string.pwd_spaziata)
+                !it.matches(Regex("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).*")) ->
+                    getString(R.string.pwd_invalida)
+                else -> null
+            }
+        }
+
+        return binding.passText.error == null
+    }
+
+    private fun validateConfirmField(): Boolean {
+        val password = binding.passText.text
+        val confirm = binding.confirmText.text
+
+        if (confirm.isBlank() || confirm != password)
+            binding.confirmText.error = getString(R.string.conferma_invalida)
+        else
+            binding.confirmText.error = null
+
+        return binding.confirmText.error == null
     }
 
     private fun showOrHideProgressBar(hide: Boolean) {
