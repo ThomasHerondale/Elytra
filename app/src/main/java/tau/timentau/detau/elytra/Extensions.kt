4036411da2e2047c76e2292fb3f36ba340c24a1a
thomas <thomasherondale@gmail.com>
2023-06-12 20:53:25 +0200

    validateMailField

--- app/src/main/java/tau/timentau/detau/elytra/Extensions.kt
+++ app/src/main/java/tau/timentau/detau/elytra/Extensions.kt
@@ -1,7 +1,9 @@
 package tau.timentau.detau.elytra
 
-import android.widget.EditText
+import android.util.Patterns
 import com.google.android.material.textfield.TextInputLayout
 
 val TextInputLayout.text: String
     get() = editText?.text.toString()
+
+fun String.isNotEmail() = !Patterns.EMAIL_ADDRESS.asPredicate().test(this)
--- app/src/main/java/tau/timentau/detau/elytra/RegisterFragment.kt
+++ app/src/main/java/tau/timentau/detau/elytra/RegisterFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import tau.timentau.detau.elytra.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
@@ -18,6 +19,21 @@ class RegisterFragment : Fragment() {
         binding = FragmentRegisterBinding.inflate(layoutInflater)
 
         return binding.root
+    }
+
+    private fun validateMailField(): Boolean {
+        var errorTextRes: Int? = null
+
+        if (binding.mailText.text.isNotEmail())
+            errorTextRes = R.string.mail_invalida
+
+            if (errorTextRes != null)
+                binding.mailText.error = getString(errorTextRes)
+            else
+                binding.mailText.error = null
+
+        // messaggio di errore vuoto -> campo corretto
+        return errorTextRes == null
     }
 
     private fun showOrHideProgressBar(hide: Boolean) {
