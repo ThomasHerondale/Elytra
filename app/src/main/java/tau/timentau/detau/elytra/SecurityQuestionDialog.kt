71ea53dad89e7e4d568ad649033c52c5fee8621b
thomas <thomasherondale@gmail.com>
2023-06-13 22:51:03 +0200

    eccezioni

--- app/src/main/java/tau/timentau/detau/elytra/SecurityQuestionDialog.kt
+++ app/src/main/java/tau/timentau/detau/elytra/SecurityQuestionDialog.kt
@@ -3,22 +3,32 @@ package tau.timentau.detau.elytra
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import tau.timentau.detau.elytra.databinding.DialogSecurityQuestionBinding
+
+private const val TAG = "SECURITY_QUESTION"
 
 class SecurityQuestionDialog : DialogFragment() {
 
     private lateinit var binding: DialogSecurityQuestionBinding
     private lateinit var handler: SecurityQuestionHandler
-    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, e ->
+        networkError()
+        Log.e(TAG, e.stackTraceToString())
+        dismiss()
+    }
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + coroutineExceptionHandler)
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -61,11 +71,14 @@ class SecurityQuestionDialog : DialogFragment() {
 
     private fun answerConfirmed() {
         val answer = binding.answerText.text
-        // non inviare risposte vuote
         if (answer.isBlank()) {
+            // segnala se la risposta è vuota e non inviarla
             binding.answerText.error = getString(R.string.inserisci_risposta)
             return
         }
+
+        // disabilita il pulsante di conferma mentre l'operazione è in corso
+        enableOrDisableConfirmButton(true)
 
         coroutineScope.launch {
             val isAnswerOkay = handler.checkAnswer(answer).await()
@@ -75,6 +88,7 @@ class SecurityQuestionDialog : DialogFragment() {
             else
                 binding.answerText.error = getString(R.string.risposta_errata)
         }
+            .invokeOnCompletion { enableOrDisableConfirmButton(false) }
     }
 
     private fun showSecurityQuestion() {
@@ -84,7 +98,7 @@ class SecurityQuestionDialog : DialogFragment() {
             val question = handler.fetchSecurityQuestion().await()
 
             binding.questionText.text = question
-            // show layout
+            // mostra le componenti quando la domanda è carica
             binding.questionProgress.visibility = View.GONE
             binding.questionText.visibility = View.VISIBLE
             binding.answerText.visibility = View.VISIBLE
@@ -96,6 +110,18 @@ class SecurityQuestionDialog : DialogFragment() {
     private fun enableOrDisableConfirmButton(disable: Boolean) {
         binding.securityQuestionDialogBottomButtons
             .positiveButton.isEnabled = !disable
+    }
+
+    private fun networkError() {
+        MaterialAlertDialogBuilder(
+            requireActivity(),
+            com.google.android.material.R.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered
+        )
+            .setTitle(R.string.errore_connessione)
+            .setMessage(R.string.imposs_connettersi_al_server)
+            .setIcon(R.drawable.ic_link_off_24)
+            .setPositiveButton(R.string.okay) { _, _ -> }
+            .show()
     }
 
     interface SecurityQuestionHandler {
