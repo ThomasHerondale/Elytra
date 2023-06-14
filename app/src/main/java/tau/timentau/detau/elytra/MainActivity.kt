54ce65128f7b02d2fec7e5f32438a6bf7315a4f2
thomas <thomasherondale@gmail.com>
2023-06-14 18:14:05 +0200

    progress dialog all'inizio

--- app/src/main/java/tau/timentau/detau/elytra/MainActivity.kt
+++ app/src/main/java/tau/timentau/detau/elytra/MainActivity.kt
@@ -2,7 +2,10 @@ package tau.timentau.detau.elytra
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.R.style.ThemeOverlay_Material3_MaterialAlertDialog_Centered
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import tau.timentau.detau.elytra.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,6 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        showProgressDialog()
         setContentView(binding.root)
+    }
+
+    private fun showProgressDialog() {
+        MaterialAlertDialogBuilder(
+            this,
+            ThemeOverlay_Material3_MaterialAlertDialog_Centered
+        )
+            .setView(R.layout.dialog_progress_simple)
+            .setCancelable(false)
+            .show()
     }
 }
