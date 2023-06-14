43da2a5430a4b7bfeebc4c8ec3e0d7b7af96e6c7
thomas <thomasherondale@gmail.com>
2023-06-14 16:17:34 +0200

    evitiamo crash inutili

--- app/src/main/java/tau/timentau/detau/elytra/MainActivity.kt
+++ app/src/main/java/tau/timentau/detau/elytra/MainActivity.kt
@@ -1,11 +1,7 @@
 package tau.timentau.detau.elytra
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import tau.timentau.detau.elytra.database.Repository
+import androidx.appcompat.app.AppCompatActivity
 import tau.timentau.detau.elytra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        CoroutineScope(Dispatchers.Main).launch {
-            val user = Repository.fetchUserData("email").await()
-            binding.textView.text = "$user"
-        }
 
         setContentView(binding.root)
     }
