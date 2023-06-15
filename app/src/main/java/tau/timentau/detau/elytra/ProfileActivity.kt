82acb77270a0377b992a807bba7835a2c00e25c0
thomas <thomasherondale@gmail.com>
2023-06-15 16:45:45 +0200

    pulsante indietro

--- app/src/main/java/tau/timentau/detau/elytra/ProfileActivity.kt
+++ app/src/main/java/tau/timentau/detau/elytra/ProfileActivity.kt
@@ -1,6 +1,7 @@
 package tau.timentau.detau.elytra
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import tau.timentau.detau.elytra.databinding.ActivityProfileBinding
 
@@ -12,6 +13,19 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
 
+        setSupportActionBar(binding.profileTopAppBar)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
         setContentView(binding.root)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressedDispatcher.onBackPressed()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
